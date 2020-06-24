34f55c34a4d4df23cc3aba34e25aa997a9dfde73
gmirsky <gmirsky@gmail.com>
2020-06-24 15:00:32 -0400

    added additional parameters

--- terraform/aws_kms_key.tf
+++ terraform/aws_kms_key.tf
@@ -1,3 +1,8 @@
 resource "aws_kms_key" "eks" {
-  description = "EKS Secret Encryption Key"
+  description             = "EKS Secret Encryption Key"
+  key_usage               = "ENCRYPT_DECRYPT"
+  deletion_window_in_days = 7
+  is_enabled              = true
+  enable_key_rotation     = false
+  tags                    = local.common_tags
 }
